216471d2cadec1f569d09ce2eafe393978d31a57
khajaasmath786 <mdkhajaasmath@gmail.com>
2015-02-08 22:29:52 +0530

    commit

---
+++ hive/src/main/java/karanUDTF/Maximum.java
@@ -0,0 +1,52 @@
+package karanUDTF;
+
+import org.apache.hadoop.hive.ql.exec.UDAFEvaluator;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.hive.ql.exec.UDAF;
+
+public class Maximum extends UDAF
+{
+  
+
+    public Maximum()
+    {
+    }
+   
+    public static class MaximumIntUDAFEvaluator
+    implements UDAFEvaluator
+{
+    private IntWritable result;
+    public void init()
+    {
+        result = null;
+    }
+
+    public boolean iterate(IntWritable value)
+    {
+        if(value == null)
+            return true;
+        if(result == null)
+            result = new IntWritable(value.get());
+        else
+            result.set(Math.max(result.get(), value.get()));
+        return true;
+    }
+
+    public IntWritable terminatePartial()
+    {
+        return result;
+    }
+
+    public boolean merge(IntWritable other)
+    {
+        return iterate(other);
+    }
+
+    public IntWritable terminate()
+    {
+        return result;
+    }
+
+  
+}
+}
